7787593b90eeae6c4f1ac0c54984396920938fa4
Steven Becker <mail-stbe@protonmail.com>
2023-12-18 21:10:46 +0100

    Dekorator um optionales Argument fuer Nachkommastellen ergaenzt.

--- classes/decorators.py
+++ classes/decorators.py
@@ -1,16 +1,21 @@
 import functools
 
-def value_formatter(func):
-    """
-    Dekorator, der eine Gleitkommazahl von einer Funktion entgegen nimmt
-    und sie in Abhaengigkeit ihrer Groesse in Exponential- oder Dezimalschreibweise zurueckgibt.
-    """
-    @functools.wraps(func)
-    def format_value(*args, **kwargs):
-        val = float(func(*args, **kwargs))
+def value_formatter(f_py=None, decimals=None):
+    assert callable(f_py) or f_py is None
+    def _decorator(func):
+        """
+        Dekorator, der eine Gleitkommazahl von einer Funktion entgegen nimmt
+        und sie in Abhaengigkeit ihrer Groesse in Exponential- oder Dezimalschreibweise zurueckgibt.
+        """
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            val = float(func(*args, **kwargs))
 
-        if abs(val) >= 1e4:
-            return f'{val:.3g}'
-        return str(val)
+            if abs(val) >= 1e4:
+                return f'{val:.3g}'
+            elif abs(val) < 1e-2:
+                return f'{val:.3g}'
+            return f'{val:.{decimals}f}' if decimals else str(val)
 
-    return format_value
+        return wrapper
+    return _decorator(f_py) if callable(f_py) else _decorator
